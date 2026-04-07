15be1f9c9c5c62f63a5feb9a87e741cafb21791c
Kavyachadha <kavyachadhaa@gmail.com>
2026-04-08 01:09:25 +0530

    Update BookMyStay.java

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,64 +1,66 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+import java.io.*;
 
-class Reservation {
-    private String guestName;
-    private String roomType;
+// UC12: Data Persistence & System Recovery
 
-    public Reservation(String guestName, String roomType) {
-        this.guestName = guestName;
-        this.roomType = roomType;
+class Reservation implements Serializable {
+    String guestName;
+    String roomType;
+
+    Reservation(String name, String type) {
+        guestName = name;
+        roomType = type;
     }
 
-    public String getGuestName() {
-        return guestName;
-    }
-
-    public String getRoomType() {
-        return roomType;
+    public String toString() {
+        return guestName + " - " + roomType;
     }
 }
 
-class BookingRequestQueue {
-    private Queue<Reservation> requestQueue;
+class PersistenceService {
 
-    public BookingRequestQueue() {
-        requestQueue = new LinkedList<>();
+    public void save(List<Reservation> list) {
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("data.ser"));
+            oos.writeObject(list);
+            oos.close();
+            System.out.println("Data saved successfully");
+        } catch (Exception e) {
+            System.out.println("Save Error: " + e.getMessage());
+        }
     }
 
-    public void addRequest(Reservation reservation) {
-        requestQueue.offer(reservation);
-    }
-
-    public Reservation getNextRequest() {
-        return requestQueue.poll();
-    }
-
-    public boolean hasPendingRequests() {
-        return !requestQueue.isEmpty();
+    public List<Reservation> load() {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("data.ser"));
+            List<Reservation> list = (List<Reservation>) ois.readObject();
+            ois.close();
+            return list;
+        } catch (Exception e) {
+            System.out.println("Load Error: " + e.getMessage());
+            return new ArrayList<>();
+        }
     }
 }
 
 public class BookMyStay {
     public static void main(String[] args) {
-        System.out.println("Booking Request Queue:");
 
-        BookingRequestQueue bookingQueue = new BookingRequestQueue();
+        List<Reservation> bookings = new ArrayList<>();
+        bookings.add(new Reservation("Abhi", "Single"));
+        bookings.add(new Reservation("Kavya", "Suite"));
 
-        Reservation r1 = new Reservation("Neha", "Single");
-        Reservation r2 = new Reservation("Suhas", "Double");
-        Reservation r3 = new Reservation("Yamrath", "Suite");
+        PersistenceService ps = new PersistenceService();
 
-        bookingQueue.addRequest(r1);
-        bookingQueue.addRequest(r2);
-        bookingQueue.addRequest(r3);
+        // Save data
+        ps.save(bookings);
 
-        while (bookingQueue.hasPendingRequests()) {
-            Reservation next = bookingQueue.getNextRequest();
-            System.out.println("Processing Request -> Guest: "
-                    + next.getGuestName()
-                    + ", Room Type: "
-                    + next.getRoomType());
+        // Load data (simulate restart)
+        List<Reservation> loaded = ps.load();
+
+        System.out.println("Recovered Bookings:");
+        for (Reservation r : loaded) {
+            System.out.println(r);
         }
     }
 }
